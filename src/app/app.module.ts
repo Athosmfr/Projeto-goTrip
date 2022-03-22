1d49759c8e17a21b9249b26f77815296b06fcf9d
aluno <Aluno@DEVE009890.educacional.up>
2022-03-21 21:12:55 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { AdminComponent } from './admin/admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    AdminComponent
   ],
   imports: [
+    MatSliderModule,
+    MatTableModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/produtos.ts
@@ -0,0 +1,15 @@
+export const Produtos = [ 
+    {
+        id: 1,
+        nomeProduto: "Produto 1",
+        descProduto: "Descrição Produto",
+        fotoProduto: '/../assets/rio.jpg'
+    },
+    {
+        id: 2,
+        nomeProduto: "Produto 2",
+        descProduto: "Descrição Produto",
+        fotoProduto: "/../assets/saopaulo.jpg"
+        
+    }
+];
